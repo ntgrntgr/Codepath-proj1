8d497990406d962434c4e34e7e69b48ffb4a496d
lacebx <87417845+lacebx@users.noreply.github.com>
2024-08-28 20:50:53 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+// script.js
+
+document.getElementById('explore-btn').addEventListener('click', function() {
+    window.location.href = 'why-computer-science.html'; // Update with the actual URL
+});
+
+document.getElementById('get-started-btn').addEventListener('click', function() {
+    window.location.href = 'preparation-tips.html'; // Update with the actual URL
+});
+
+// Add event listeners for feature sections
+document.getElementById('career-paths').addEventListener('click', function() {
+    window.location.href = 'career-paths.html'; // Update with the actual URL
+});
+
+document.getElementById('job-opportunities').addEventListener('click', function() {
+    window.location.href = 'job-opportunities.html'; // Update with the actual URL
+});
+
+document.getElementById('preparation-tips').addEventListener('click', function() {
+    window.location.href = 'preparation-tips.html'; // Update with the actual URL
+});
